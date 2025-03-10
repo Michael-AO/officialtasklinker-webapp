d6b994299fa56657b91a6333d93b04354de038dd
Michaelasereo <Michaelasereo@gmail.com>
2025-03-10 07:47:06 +0100

    Updated project with latest changes

--- src/Tabs/ProfileUI.jsx
+++ src/Tabs/ProfileUI.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import styles from "./ProfileUI.module.css";
 import { auth } from "../firebase"; // Ensure Firebase is properly configured
 import Authentication from "../components/authentication";
-import DefaultProfile from "../components/defaultProfile";
+import DefaultProfile from "../components/DefaultProfile";
 
 function ProfileUI() {
   const [user, setUser] = useState(null);
